06bd87c05aa84a65d9f8212c3ee1eb7aff6467e3
Thomas Cranny <Thom@sCranny.net>
2015-07-05 11:57:29 +1000

    Fixed map loading.

--- bcc_nav/static/app/app.js
+++ bcc_nav/static/app/app.js
@@ -4,11 +4,11 @@ var app = angular.module('BCCNavApp', [])
     .controller('BCCNavCtrl', ['$scope', function($scope) {
 
         $scope.utilities = [
-            {title: 'Bike Racks', key: 'bikeracks', display: true, markers: bikerackdata},
+            {title: 'Bike Racks', key: 'bikeracks', display: false, markers: bikerackdata},
             {title: 'Bus Stops', key: 'bus_stops', display: false, markers: busstopdata},
             {title: 'Ferry Terminals', key: 'ferries', display: false, markers: ferriesdata},
             {title: 'Libraries', key: 'libraries', display: false, markers: librariesdata},
-            {title: 'Wifi Hotspots', key: 'wifi', display: true, markers: wifidata}
+            {title: 'Wifi Hotspots', key: 'wifi', display: false, markers: wifidata}
         ];
 
         $scope.toggleUtility = function (utility) {
@@ -24,10 +24,8 @@ var app = angular.module('BCCNavApp', [])
                     allmarkers.push.apply(allmarkers, $scope.utilities[i].markers);
                 }
             }
-            console.log('Redrew!');
-            renderMarkers();
         };
-
+        console.log('Scope!');
     }]);
 
 /* Modify angular to not conflict with Jinja template tags. */
